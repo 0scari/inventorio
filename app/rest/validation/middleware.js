f78265372154155684a038ea04ef181572a9e990
0scari <pozdanko@uni.coventry.ac.uk>
2020-11-29 17:16:37 +0000

    fix validation middleware bug

--- app/rest/validation/middleware.js
+++ app/rest/validation/middleware.js
@@ -4,7 +4,7 @@ const status = require('http-status-codes')
 const {storeSchema, validate} = require('../validation/schema/store')
 
 module.exports = async(req, res, next) => {
-    if (req.method = 'POST' || req.method == 'PUT') {
+    if (req.method == 'POST' || req.method == 'PUT') {
         if (!req.headers['content-type'] && req.headers['content-type'] != 'application/json') {
             res.status(status.UNSUPPORTED_MEDIA_TYPE).end()
         } else if (!validate(storeSchema, req.body.value)) {
