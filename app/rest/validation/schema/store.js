0a52917ae7103709c8ff594fd8b5b0290fc62d8b
0scari <pozdanko@uni.coventry.ac.uk>
2020-11-29 17:01:33 +0000

    add PUT and POST request validation

--- app/rest/validation/schema/store.js
+++ app/rest/validation/schema/store.js
@@ -1,7 +1,18 @@
 'use strict'
 
-module.exports = {
-    id: 'string(1,25)',
-    strField: String,
-    numField: 'numeric(1,)'
+const validate = require('validate-fields')()
+
+validate.typedef('Activity', {
+    'ItemName': 'string(1,40)',
+    'ItemId?': `number(1${'0'.repeat(11)},${'9'.repeat(12)})`,
+    'Quantity?': 'number(1,)'
+})
+
+module.exports.storeSchema = {
+    StoreId: `number(1${'0'.repeat(4)},${'9'.repeat(5)})`,
+    'Refund?': ['Activity'],
+    'Delivery?': ['Activity'],
+    'Sale?': ['Activity']
 }
+
+module.exports.validate = validate
