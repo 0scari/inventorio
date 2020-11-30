20dd510666f71e50cb9b16eef1db49c347c8a3f0
Oskars Pozdnakovs <pozdnako@uni.coventry.ac.uk>
2020-11-30 12:07:17 +0000

    Delete fs.js

--- app/__mocks__/fs.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-// const fs = jest.genMockFromModule('fs');
-
-module.exports.readFileSync = () => true
-module.exports.writeFileSync = () => true
-module.exports.createReadStream = jest.fn()
-
-// module.exports = fs
